d9c7affc905557e2314def98f4978266739e39c1
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2020-12-04 18:33:50 +0530

    Added summary of chapter_03

--- part_1/chapter_03/src/gen.js
+++ part_1/chapter_03/src/gen.js
@@ -252,6 +252,21 @@ function growingFunctions() {
     printFarmInventory(7, 16, 3);
 }
 
+function summary() {
+    // Define f to hold a function value
+    const f = function(a) {
+        console.log(a + 2);
+    };
+
+    // Declare g to be a function
+    function g(a, b) {
+        return a * b * 3.5;
+    }
+
+    // A less verbose function value
+    let h = a => a % 3;
+}
+
 // definingFunction();
 // bindingsScopes();
 // nestedScope();
