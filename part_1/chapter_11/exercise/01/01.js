775ae2cbdd299ec3f2338f60d3adae2c5b916991
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2021-01-22 17:37:08 +0530

    chapter_11 2 exercises complete

--- part_1/chapter_11/exercise/01/01.js
+++ part_1/chapter_11/exercise/01/01.js
@@ -1,1 +1,36 @@
 /* Tracking the Scalpel */
+
+async function locateScalpel(nest) {
+  let current = nest.name;
+  let next = await storage(nest, "scalpel");
+  while (true) {
+    if (current === next) {
+      return current;
+    }
+    current = next;
+    next = await anyStorage(nest, next, "scalpel");
+  }
+}
+
+function locateScalpel(nest) {
+  return new Promise((resolve, reject) => {
+    let current = nest.name;
+
+    function locator(current, next) {
+      if (current === next) {
+        resolve(current);
+      } else {
+        let current = next;
+        anyStorage(nest, next, "scalpel")
+          .then(next => {
+            locator(current, next);
+          });
+      }
+    }
+
+    storage(nest, "scalpel")
+      .then(next => {
+        locator(nest.name, next);
+      });
+  });
+}
